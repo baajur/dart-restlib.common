bc1d9ce397d2a502a025702dd31e59116b31f955
David Bordoley <bordoley@gmail.com>
2013-10-17 19:30:49 -0700

    Add a test for each expect clause and wrap test in groups.

--- test/preconditions_test.dart
+++ test/preconditions_test.dart
@@ -2,34 +2,44 @@ import "package:unittest/unittest.dart";
 import "package:restlib_common/preconditions.dart";
 
 preconditionsTests() {
-  test("checkArgument", () {
-    expect(checkArgument(true), isNull);
-    expect(() => checkArgument(false), throwsArgumentError);
+  group("checkArgument", () {
+    test("with true", () => 
+        expect(checkArgument(true), isNull));
+    test("with false", () => 
+        expect(() => checkArgument(false), throwsArgumentError));
+  });
+
+  group("checkRangeInclusive", () {
+    test("with value below range", () => 
+        expect(() => checkRangeInclusive(-2, -1, 1), throwsRangeError));
+    test("with floor value", () =>
+        expect(checkRangeInclusive(-1, -1, 1), isNull));
+    test("with value in range", () =>
+        expect(checkRangeInclusive(0, -1, 1), isNull));
+    test("with ceiling value", () => 
+        expect(checkRangeInclusive(1, -1, 1), isNull));
+    test("with value above range", () =>
+        expect(() => checkRangeInclusive(2, -1, 1), throwsRangeError));
+    test("with low == high", () => 
+        expect(() => checkRangeInclusive(0, 1, 1), throwsArgumentError));
+    test("with low > high", () => 
+        expect(() => checkRangeInclusive(0, 2, 1), throwsArgumentError));
   });
   
-  test("checkRangeInclusive", () {
-    expect(() => checkRangeInclusive(-2, -1, 1), throwsRangeError);
-    
-    expect(checkRangeInclusive(-1, -1, 1), isNull);
-    expect(checkRangeInclusive(0, -1, 1), isNull);
-    expect(checkRangeInclusive(1, -1, 1), isNull);
-    
-    expect(() => checkRangeInclusive(2, -1, 1), throwsRangeError);
-    
-    expect(() => checkRangeInclusive(0, 1, 1), throwsArgumentError);
-    expect(() => checkRangeInclusive(0, 2, 1), throwsArgumentError);    
+  group("checkNotNull", () {
+    test("with null", () => 
+        expect(() => checkNotNull(null), throwsArgumentError));
+    test("with non-null object", () {
+      final Object testObj = new Object();
+      expect(checkNotNull(testObj), equals(testObj));
+    });
   });
   
-  test("checkNotNull", () {
-    expect(() => checkNotNull(null), throwsArgumentError);
-    
-    final Object testObj = new Object();
-    expect(checkNotNull(testObj), equals(testObj));
-  });
-  
-  test("checkState", () {
-    expect(checkState(true), isNull);
-    expect(() => checkState(false), throwsStateError);
+  group("checkState", () {
+    test("with true", () => 
+        expect(checkState(true), isNull));
+    test("with false", () => 
+        expect(() => checkState(false), throwsStateError));
   });
 }
 
