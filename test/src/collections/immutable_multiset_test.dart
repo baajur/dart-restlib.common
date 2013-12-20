b90c63731beca5fbce5c67434d531556d444fcf4
David Bordoley <bordoley@gmail.com>
2013-12-20 01:14:58 -0800

    add tests for CopyOnWriteMultiset.

--- test/src/collections/immutable_multiset_test.dart
+++ test/src/collections/immutable_multiset_test.dart
@@ -4,16 +4,26 @@ void immutableMultisetTests() {
   new EqualsTester()
   ..addEqualityGroup(
       [Persistent.EMPTY_MULTISET,
-       Persistent.EMPTY_MULTISET.add("a").remove("a")])
+       Persistent.EMPTY_MULTISET.add("a").remove("a"),
+       (new CopyOnWriteMultisetBuilder()).build()])
   ..addEqualityGroup(
       [Persistent.EMPTY_MULTISET.add("a"),
-       Persistent.EMPTY_MULTISET.add("a")])     
+       Persistent.EMPTY_MULTISET.add("a"),
+       (new CopyOnWriteMultisetBuilder()..add("a")).build()])     
   ..executeTestCase();
   
   group("persistent", () => 
       new ImmutableMultisetTester()
         ..generator = ((final int size) => 
             Persistent.EMPTY_MULTISET.addAll(new List.generate(size, (i) => i)))
+        ..elementGenerator = new SequenceElementGenerator()
+        ..testSizes = [0,1,1000]
+        ..testImmutableMultiset());
+  
+  group("copyonwrite", () => 
+      new ImmutableMultisetTester()
+        ..generator = ((final int size) => 
+            (new CopyOnWriteMultisetBuilder()..addAll(new List.generate(size, (i) => i))).build())
         ..elementGenerator = new SequenceElementGenerator()
         ..testSizes = [0,1,1000]
         ..testImmutableMultiset());
