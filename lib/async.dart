113dcc8e88efa5e29ec793913b5f1fdd68442a7d
David Bordoley <bordoley@gmail.com>
2014-01-14 01:37:56 -0800

    Async stream convenience methods.

---
+++ lib/async.dart
@@ -0,0 +1,36 @@
+library restlib.common.async;
+
+import "dart:async";
+
+abstract class ByteStreamable {
+  Stream<List<int>> asByteStream();
+}
+
+Stream valueAsStream(dynamic value) =>
+    new Stream.fromFuture(new Future.value(value));
+
+Stream concatStreams(final Iterable<Stream> streams) {
+  final StreamController controller = new StreamController();
+  final Iterator<Stream> streamItr = streams.iterator;
+  
+  void addNextStream() {
+    final Stream stream = streamItr.current;
+    stream.listen(controller.add)
+      ..onError(controller.addError)
+      ..onDone(() {
+        if(streamItr.moveNext()) {
+          addNextStream();
+        } else {
+          controller.close();
+        }
+      });
+  }
+  
+  if (streamItr.moveNext()) {
+    addNextStream();
+  } else {
+    controller.close();
+  }
+  
+  return controller.stream;
+}    
