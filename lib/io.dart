53142a7389a18dcc256fa900cf5d1aec352fd783
David Bordoley <bordoley@gmail.com>
2014-01-14 17:41:55 -0800

    simplify bytestream definitions.

--- lib/io.dart
+++ lib/io.dart
@@ -6,81 +6,26 @@ import "collections.dart";
 import "objects.dart";
 import "preconditions.dart";
 
-abstract class ByteStreamable {      
+abstract class ByteStreamable {  
+  factory ByteStreamable.byteRange(final ByteRangeable delegate, [final int start, final int end]) =>
+      new _ByteRange(delegate, firstNotNull(start, 0), new Option(end));
   Stream<List<int>> asByteStream();
 }
 
-abstract class ByteRange implements ByteStreamable {
-  factory ByteRange(final ByteSubRangeable delegate, [final int start, final int end]) =>
-      new _ByteSubRange(delegate, firstNotNull(start, 0), new Option(end));
-  
-  Future<int> length();
-  ByteRange subRange(int start, int end);
-}
-
-abstract class ByteSubRangeable {  
+abstract class ByteRangeable {  
   Future<int> length();
   Stream<List<int>> openRead([int start, int end]);
 }
 
-class _ByteSubRange implements ByteRange, ByteSubRangeable {
-  final ByteSubRangeable delegate;
+class _ByteRange implements ByteStreamable {
+  final ByteRangeable delegate;
   final int start;
   final Option<int> end;
   
-  _ByteSubRange(this.delegate, this.start, this.end) {
+  _ByteRange(this.delegate, this.start, this.end) {
     checkNotNull(start);
   }
   
-  Stream<List<int>> openRead([int start, int end]) {
-    start = firstNotNull(start, 0);
-    checkArgument(start >= 0);
-    
-    Option<int> endOption = 
-        new Option(end)
-          .orElse(this.end.map((final int end) =>
-              end - this.start));
-    
-    endOption.map((final int end) => 
-        checkArgument(end >= 0));
-    
-    final int newStart = this.start + start;
-    final Option<int> newEnd = 
-        endOption.map((final int end) =>
-            this.start + end);
-    newEnd.map((final int end) {
-      checkArgument(newStart <= end);
-      this.end.map((final int oldEnd) => 
-          checkArgument(end <= oldEnd));
-    });
-
-    
-    return delegate.openRead(newStart, newEnd.nullableValue);
-  }
-  
-  Future<int> length() =>
-      end
-        .map((final int end) => 
-            new Future.value(end - start))
-        .orCompute(() => 
-            delegate.length()
-              .then((final int length) =>
-                  length - start));
-  
-  ByteRange subRange(final int start, final int end) {
-    checkArgument(start >= 0);
-    checkArgument(end >= 0);
-    
-    final int newStart = this.start + start;
-    final int newEnd = this.start + end;
-    
-    checkArgument(newStart <= newEnd);
-    this.end.map((final int end) =>
-        checkArgument(newEnd <= end));
-    
-    return new ByteRange(delegate, newStart, newEnd);
-  }
-  
   Stream<List<int>> asByteStream() =>
-      openRead();
+      delegate.openRead(start, end.nullableValue);
 }
