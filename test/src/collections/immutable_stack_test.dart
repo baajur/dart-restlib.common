f8a98e170878286e5a4f651873ddc0342749a3b3
David Bordoley <bordoley@gmail.com>
2014-02-16 03:09:33 -0800

    add missing test commit.

--- test/src/collections/immutable_stack_test.dart
+++ test/src/collections/immutable_stack_test.dart
@@ -2,14 +2,14 @@ part of restlib.common.collections_test;
 
 immutableStackTests() {
   new EqualsTester()
-    ..addEqualityGroup([Persistent.EMPTY_STACK, Persistent.EMPTY_STACK.pushAll([]), Persistent.EMPTY_STACK.push("a").tail])
-    ..addEqualityGroup([Persistent.EMPTY_STACK.pushAll(["a", "b", "c"]), Persistent.EMPTY_STACK.pushAll(["a", "b", "c"]), Persistent.EMPTY_STACK.pushAll(["a", "b", "c", "d"]).tail])
+    ..addEqualityGroup([EMPTY_STACK, EMPTY_STACK.pushAll([]), EMPTY_STACK.push("a").tail])
+    ..addEqualityGroup([EMPTY_STACK.pushAll(["a", "b", "c"]), EMPTY_STACK.pushAll(["a", "b", "c"]), EMPTY_STACK.pushAll(["a", "b", "c", "d"]).tail])
     ..executeTestCase();
   
   group("persistent", () =>
       new ImmutableStackTester()
         ..testSizes = [0, 1, 1000]
         ..generator = ((final int size) => 
-            Persistent.EMPTY_STACK.pushAll(new List.generate(size, (i) => i)))
+            EMPTY_STACK.pushAll(new List.generate(size, (i) => i)))
         ..testImmutabletStack());
 }
