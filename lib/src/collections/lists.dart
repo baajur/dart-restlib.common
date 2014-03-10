6dc700a5a43878d7d9fd49c55e7551cd5c568608
David Bordoley <bordoley@gmail.com>
2014-03-09 18:33:05 -0700

    Add sublist function.

--- lib/src/collections/lists.dart
+++ lib/src/collections/lists.dart
@@ -1,16 +1,43 @@
 part of collections;
 
-
 List concatLists(final Iterable<List> lists) =>
     new _ConcatList(checkNotNull(lists.toList(growable: false)));
 
-class _ConcatList<T> extends ListBase<T> implements List<T> {
-  final List<List<T>> _lists;
+List subList(final List list, [int start, int length]) {
+  checkNotNull(list);
+  start = firstNotNull(start, 0);
+  length = firstNotNull(length, list.length);
+
+  checkArgument(start >= 0 && start < list.length);
+  checkArgument(length <= list.length - start);
+
+  return new _SubList(list, start, length);
+}
+
+class _SubList<T> extends ListBase<T> implements List<T> {
+  final List<T> _delegate;
+  final int _start;
   final int length;
 
-  _ConcatList(final List<List<T>> lists) :
-    this._lists = lists,
-    length = lists.fold(0, (final int acc, final List next) => acc + next.length);
+  _SubList(this._delegate, this._start, this.length);
+
+  void set length(int length) =>
+      throw new UnsupportedError("List is unmodifiable");
+
+  void operator[]=(final int index, final T value) =>
+      throw new UnsupportedError("List is unmodifiable");
+
+  T operator[](final int index) =>
+      _delegate[_start + index];
+}
+
+class _ConcatList<T> extends ListBase<T> implements List<T> {
+  final List<List<T>> _lists;
+
+  _ConcatList(this._lists);
+
+  int get length =>
+      _lists.fold(0, (final int acc, final List next) => acc + next.length);
 
   void set length(int length) =>
       throw new UnsupportedError("List is unmodifiable");
