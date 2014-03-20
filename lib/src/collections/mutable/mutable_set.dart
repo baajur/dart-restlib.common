70ba98fccace98545814c1ed092b6e813d8a2d3c
David Bordoley <bordoley@gmail.com>
2014-03-19 21:53:41 -0700

    don't define the type of classes returned by set joins.

--- lib/src/collections/mutable/mutable_set.dart
+++ lib/src/collections/mutable/mutable_set.dart
@@ -4,57 +4,53 @@ abstract class MutableSet<E> extends MutableCollection<E> implements FiniteSet<E
   factory MutableSet.hash({final Iterable<E> elements}) =>
       (elements != null) ?
           elements.fold(
-              new _MutableDictionaryBackedSet(new MutableDictionary.hash()), 
-              (final MutableSet<E> accumulator, final E element) => 
+              new _MutableDictionaryBackedSet(new MutableDictionary.hash()),
+              (final MutableSet<E> accumulator, final E element) =>
                   accumulator..add(element)) :
             new _MutableDictionaryBackedSet(new MutableDictionary.hash());
-  
+
   factory MutableSet.splayTree({final Iterable<E> elements}) =>
       (elements != null) ?
           elements.fold(
-              new _MutableDictionaryBackedSet(new MutableDictionary.splayTree()), 
-              (final MutableSet<E> accumulator, final E element) => 
+              new _MutableDictionaryBackedSet(new MutableDictionary.splayTree()),
+              (final MutableSet<E> accumulator, final E element) =>
                   accumulator..add(element)) :
             new _MutableDictionaryBackedSet(new MutableDictionary.splayTree());
-  
-  MutableSet<E> difference(FiniteSet<E> other);
-  MutableSet<E> intersection(FiniteSet<Object> other);
-  MutableSet<E> union(FiniteSet<E> other);
 }
 
-class _MutableDictionaryBackedSet<E> 
-    extends DictionaryBackedSet<E> 
-    implements MutableSet<E> {   
+class _MutableDictionaryBackedSet<E>
+    extends DictionaryBackedSet<E>
+    implements MutableSet<E> {
   final MutableDictionary<E,E> delegate;
-  
+
   _MutableDictionaryBackedSet(this.delegate);
-  
+
   void add(final E element) =>
       delegate.put(element, element);
-  
+
   void addAll(final Iterable<E> elements) =>
-      elements.forEach((final E element) => 
+      elements.forEach((final E element) =>
           add(element));
-  
+
   void clear() =>
       delegate.clear();
-  
+
   MutableSet<E> difference(FiniteSet<E> other) =>
     // FIXME: Probaly should make the return type implementation specific
     new MutableSet.hash()..addAll(
-        this.where((final E element) => 
+        this.where((final E element) =>
             !other.contains(element)));
 
   MutableSet<E> intersection(FiniteSet<Object> other) =>
     // FIXME: Probaly should make the return type implementation specific
     new MutableSet.hash()..addAll(
-        this.where((final E element) => 
+        this.where((final E element) =>
             other.contains(element)));
-  
+
   MutableSet<E> union(FiniteSet<E> other) =>
     // FIXME: Probaly should make the return type implementation specific
     new MutableSet.hash()..addAll(this)..addAll(other);
-  
+
   Option<E> remove(final E element) =>
       delegate.removeAt(element);
 }
