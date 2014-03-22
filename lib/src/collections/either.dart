74c4cd76fc579b5703170d6aba9358e7c94d2396
David Bordoley <bordoley@gmail.com>
2014-03-21 17:51:27 -0700

    Add const constructors for either.

--- lib/src/collections/either.dart
+++ lib/src/collections/either.dart
@@ -2,10 +2,13 @@ part of collections;
 
 abstract class Either<L, R> {
   factory Either.leftValue(final L value) =>
-      new _EitherLeft(Tuple.create1(value));
+      new _Left(Tuple.create1(value));
 
   factory Either.rightValue(final R value) =>
-      new _EitherRight(Tuple.create1(value));
+      new _Right(Tuple.create1(value));
+
+  const factory Either.leftConstant(final Some<L> value) = _Left;
+  const factory Either.rightConstant(final Some<R> value) = _Right;
 
   Option<L> get left;
   Option<R> get right;
@@ -15,6 +18,8 @@ abstract class Either<L, R> {
 }
 
 abstract class _Either<L,R> implements Either<L,R> {
+  const _Either();
+
   int get hashCode => computeHashCode([left, right]);
 
   bool operator==(final other) {
@@ -33,10 +38,10 @@ abstract class _Either<L,R> implements Either<L,R> {
              (final R right) => "Either.right($right)");
 }
 
-class _EitherLeft<L,R> extends _Either<L,R> {
+class _Left<L,R> extends _Either<L,R> {
   final Some<L> left;
 
-  _EitherLeft(this.left);
+  const _Left(this.left);
 
   Option<R> get right =>
       Option.NONE;
@@ -47,10 +52,10 @@ class _EitherLeft<L,R> extends _Either<L,R> {
       onLeft(left.e0);
 }
 
-class _EitherRight<L,R> extends _Either<L,R> {
+class _Right<L,R> extends _Either<L,R> {
   final Some<R> right;
 
-  _EitherRight(this.right);
+  const _Right(this.right);
 
   Option<L> get left =>
       Option.NONE;
